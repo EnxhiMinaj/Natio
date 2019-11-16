6ae8889fdce05e45ecc40a0c159148795cb1bd36
Edison Rama <edison.r@scopicsoftware.com>
2019-11-16 13:07:26 +0200

    initial commit

---
+++ src/test/java/com/junction/natio/NatioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.junction.natio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NatioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
