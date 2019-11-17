c04cd9349c2d890555868ac507736fb4791f7de4
Enxhi <enxhi.minaj@student.lut.fi>
2019-11-17 08:13:08 +0200

    Chat + Channels

--- ui/src/main/natio-ui/src/app/all-view/chats/chats.module.ts
+++ ui/src/main/natio-ui/src/app/all-view/chats/chats.module.ts
@@ -3,14 +3,12 @@ import {CommonModule} from '@angular/common';
 import {ChatsComponent} from './chats.component';
 import {CustomMaterialModule} from "../../core/module/CustomMaterialModule";
 import {ChatsRoutingModule} from "./chats-routing.module";
-import {ChartsGraphModule} from "../charts/charts.module";
 
 @NgModule({
   imports: [
     CommonModule,
     CustomMaterialModule,
     ChatsRoutingModule,
-    ChartsGraphModule
   ],
   declarations: [ChatsComponent],
   providers: [
--- ui/src/main/natio-ui/src/app/all-view/layout-routing.module.ts
+++ ui/src/main/natio-ui/src/app/all-view/layout-routing.module.ts
@@ -1,7 +1,7 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
 import {LayoutComponent} from "./layout.component";
-import {LOCATION_POINT_DETAILS, CHATS, MAIN_URL, MY_TRIPS, SUGGESTIONS, NATURE_LOCATION} from "../core/utility/navigation-url";
+import {CHARTS, CHATS, MAIN_URL, MY_TRIPS, SUGGESTIONS, NATURE_LOCATION, LOCATION_POINT_DETAILS, CHAT_CHANNEL} from "../core/utility/navigation-url";
 
 const routes: Routes = [
   {
@@ -12,9 +12,11 @@ const routes: Routes = [
       {path: MAIN_URL, loadChildren: './explore-trails/explore-trails.module#ExploreTrailsModule'},
       {path: MY_TRIPS, loadChildren: './my-trips/my-trips.module#MyTripsModule'},
       {path: SUGGESTIONS, loadChildren: './suggestions/suggestions.module#SuggestionsModule'},
-      {path: CHATS, loadChildren: './chats/chats.module#ChatsModule'},
       {path: LOCATION_POINT_DETAILS, loadChildren: './charts/charts.module#ChartsGraphModule'},
+      {path: CHATS, loadChildren: './group-channels/group-channels.module#GroupChannelsModule'},
+      {path: CHARTS, loadChildren: './charts/charts.module#ChartsGraphModule'},
       {path: NATURE_LOCATION, loadChildren: './nature-location/nature-location.module#NatureLocationModule'},
+      {path: CHAT_CHANNEL, loadChildren: './chats/chats.module#ChatsModule'}
     ]
   }
 ];
