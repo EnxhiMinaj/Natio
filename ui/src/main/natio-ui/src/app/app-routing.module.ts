6f6ddb6ea84c2275f42cf2ac80a19a37cbdfa843
Enxhi <enxhi.minaj@student.lut.fi>
2019-11-17 08:21:59 +0200

    login auth

--- ui/src/main/natio-ui/src/app/app-routing.module.ts
+++ ui/src/main/natio-ui/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import {AuthGuard} from "./core/lib/services/auth-guard.service";
 
 const routes: Routes = [
   {path: '', redirectTo: 'natio', pathMatch: 'full'},
-  {path: 'natio', loadChildren: './all-view/layout.module#LayoutModule'},
+  {path: 'natio', loadChildren: './all-view/layout.module#LayoutModule', canLoad: [AuthGuard]},
   {path: 'login', loadChildren: './login/login.module#LoginModule'},
   {path: 'sign-up', loadChildren: './sign-up/sign-up.module#SignUpModule'}
 ];
