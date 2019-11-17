8b9b43101eaf80ead7a90ba5c35311cfdbb77d52
Anil Kumal <anil.kumal@student.lut.fi>
2019-11-17 06:38:12 +0200

    pwa implemented

--- ui/src/main/natio-ui/src/app/app.component.ts
+++ ui/src/main/natio-ui/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {SwUpdate} from "@angular/service-worker";
+import {MatSnackBar} from "@angular/material";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,62 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'natio-ui';
+  title = 'Natio';
+  promptEvent: any;
+
+  constructor(private swUpdate: SwUpdate, private snackBar: MatSnackBar) {
+  }
+  ngOnInit() {
+    this.notifyNewVersion();
+    this.pwaFunctions();
+  }
+
+  /**
+   * This method notifies the user about new version of application
+   */
+  notifyNewVersion(){
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm("New version available. Load New Version?")) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+
+  pwaFunctions() {
+    let noInternetSnack;
+
+    window.addEventListener('online', () => {
+      noInternetSnack.dismiss();
+    });
+
+    window.addEventListener('offline', () => {
+      noInternetSnack = this.snackBar.open('No Internet connection', 'Ok');
+    });
+
+    if (this.swUpdate.isEnabled) {
+      // this.snackBar.open('Service Workers enabled', 'OK', {duration: 3000});
+      console.log("Server Workers Enabled.");
+    }
+
+    //ask user to install pwa application
+    window.addEventListener('beforeinstallprompt', event => {
+      event.preventDefault();
+
+      this.promptEvent = event;
+      const snackInstall = this.snackBar.open('Do you want to install the application to your device ?', 'Install', {duration: 6000});
+
+      snackInstall.onAction().subscribe(() => {
+        this.promptEvent.prompt();
+        this.promptEvent.userChoice.then(choiceResult => {
+          if (choiceResult.outcome === 'accepted') {
+            this.snackBar.open('PWA install accepted', 'OK', {duration: 3000});
+          } else {
+            this.snackBar.open('PWA install dismissed', 'OK', {duration: 3000});
+          }
+        });
+      });
+    });
+  }
 }
